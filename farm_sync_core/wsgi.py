b5026b8d4d52c2ce19ba27382784c818fb608692
Bhadra2811 <bhadranair2811@gmail.com>
2025-04-25 21:33:47 +0530

    frontend-added

--- farm_sync_core/wsgi.py
+++ farm_sync_core/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for greenvest_core project.
+WSGI config for farm_sync_core project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greenvest_core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farm_sync_core.settings')
 
 application = get_wsgi_application()
